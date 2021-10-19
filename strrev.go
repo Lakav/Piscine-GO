fe5c77d39e3db5286d48e85aa11b93e5c6c3e909
julescavanier <jules.cavanier@ynov.com>
2021-10-19 11:25:56 +0200

    ok

--- strrev.go
+++ strrev.go
@@ -1,11 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StrRev(s string) string {
 	var a string
 	for i := len(s); i > 0; i-- {
-		z01.PrintRune(rune(s[i-1]))
+		a += string(s[i-1])
 	}
 	return a
 }
